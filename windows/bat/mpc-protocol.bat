2f6ce73379baabb99fd3765642d09b70f38449bf
northsea4 <ns4@github.io>
2024-11-25 12:47:42 +0800

    feat: support broken url

--- windows/bat/mpc-protocol.bat
+++ windows/bat/mpc-protocol.bat
@@ -1,6 +1,8 @@
+@echo off
 Setlocal EnableDelayedExpansion
-set url=%~1
-set url=!url: =%%20!
+
+set "url=%~1"
+echo Input URL: !url!
 
 @REM 工作目录是在mpc-hc目录下的，所以用`%~dp0`来获取当前目录
 
@@ -21,5 +23,27 @@ if exist "%~dp0mpc-hc64.exe" (
 )
 
 REM mpc-hc://
-@REM start "MPC" "%~dp0vlc.exe" --open "%url:~9%"
-start "MPC" "%~dp0%mpc_exe%" "%url:~9%"
+@REM start "MPC" "%~dp0mpc-hc.exe" --open "%url:~9%"
+
+:: Remove mpc-hc:// prefix
+set "url=!url:mpc-hc://=!"
+echo After removing prefix: !url!
+
+:: Fix Chrome 130+ format
+set "needfix="
+echo !url! | findstr /i "^http//" >nul && set "needfix=1"
+echo !url! | findstr /i "^https://" >nul && set "needfix=1"
+if defined needfix (
+    echo Fixing Chrome 130+ format
+    set "url=!url:http//=http://!"
+    set "url=!url:https//=https://!"
+    echo After fixing: !url!
+)
+
+:: Ensure spaces are encoded
+set "url=!url: =%%20!"
+echo Final URL: !url!
+
+:: Launch MPC
+echo Launching MPC...
+start "MPC" "%~dp0%mpc_exe%" "!url!"
